c01d24ccbbb29517331c20e23d57813fe73bf9a8
Katie Lonsdale <katie952@gmail.com>
2022-12-09 09:47:49 -0700

    Add define and id

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,28 @@
 
 # YOU DO the rest:
 
-
+#The include? method is called on the string object "Hello World"
+#The method checks if the argument that is passed is included in the string it was called on.
+#It returns a boolean value. The expected return here is true.
 "Hello World".include?("Hello")
 
-
+#The end_with? method is called on the string object "Hello World"
+#The method checks if the argument passed is at the end of the string it was called on. If multiple arguments are passed, 
+#it will return true if any of them match.
+# It returns a boolean value. The expected return here is false.
 "Hello World".end_with?("Hello")
 
-
+#The expected return here is true.
 "Hello World".end_with?("rld")
 
-
+#The even? method is called on the integer object 12.
+#The method checks if the object it is called on is an even number. It does not require arguments.
+#It returns a boolean value. The expected return here is true.
 12.even?
 
-
+#The next method is called on the integer object 18.
+# The method returns the next consecutive integer. It does not require arguments.
+#It returns an integer value. The expected return here is 19.
 18.next
 
 
